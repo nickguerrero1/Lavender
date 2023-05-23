c9bcc1e3fe9b9f8319c49869840943ee550d11ec
Nicholas Guerrero <nicholasguerrero@Nicholass-MacBook-Air.local>
2023-05-23 18:45:59 -0400

    login

--- Lavender/ContentView.swift
+++ Lavender/ContentView.swift
@@ -8,15 +8,81 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var username = ""
+    @State private var password = ""
+    @State private var wrongUsername = 0
+    @State private var wrongPassword = 0
+    @State private var showingLoginScreen = false
+    
     var body: some View {
-        VStack{
-            Image("Lavender")
-                .resizable()
-                .frame(width: 250, height: 250)
-                .cornerRadius(50)
-                .padding(.bottom, 60)
-            Text("Lavender")
-                .font(.title)
+        NavigationView{
+            ZStack{
+                Color.blue
+                    .ignoresSafeArea()
+                Circle()
+                    .scale(1.7)
+                    .foregroundColor(.white.opacity(0.15))
+                Circle()
+                    .scale(1.35)
+                    .foregroundColor(.white)
+                VStack{
+                    Text("Login")
+                        .font(.largeTitle)
+                        .bold()
+                        .padding()
+                    TextField("Username", text: $username)
+                        .padding()
+                        .frame(width: 300, height: 50)
+                        .background(.black.opacity(0.05))
+                        .cornerRadius(10)
+                        .border(.red, width: CGFloat(wrongUsername))
+                    SecureField("Password", text: $password)
+                        .padding()
+                        .frame(width: 300, height: 50)
+                        .background(.black.opacity(0.05))
+                        .cornerRadius(10)
+                        .border(.red, width: CGFloat(wrongPassword))
+                    Button("Login"){
+                        authenticateUser(username: username, password: password)
+                    }
+                        .foregroundColor(.white)
+                        .frame(width: 300, height: 50)
+                        .background(Color.blue)
+                        .cornerRadius(10)
+                    
+                    NavigationLink(destination:
+                        VStack{
+                            Text("You are logged in @\(username)")
+                                .padding(.bottom, 0)
+                            Spacer()
+                            Image("Lavender")
+                                .resizable()
+                                .frame(width: 250, height: 250)
+                                .cornerRadius(50)
+                                .padding(.bottom, 60)
+                            Text("Lavender")
+                                .font(.title)
+                            Spacer()
+                        }
+                        , isActive: $showingLoginScreen){
+                            EmptyView()
+                    }
+                }
+            }
+            .navigationBarHidden(true)
+        }
+    }
+    func authenticateUser(username: String, password: String){
+        if username.lowercased() == "meaply" {
+            wrongUsername = 0
+            if password.lowercased() == "123" {
+                wrongPassword = 0
+                showingLoginScreen = true
+            } else{
+                wrongPassword = 2
+            }
+        } else{
+            wrongUsername = 2
         }
     }
 }
