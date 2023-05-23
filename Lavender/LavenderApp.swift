f4300a9ff6a9e2bf61a105e766085c5407f13ed1
Nicholas Guerrero <nicholasguerrero@Nicholass-MacBook-Air.local>
2023-05-23 15:37:28 -0400

    first commit

---
+++ Lavender/LavenderApp.swift
@@ -0,0 +1,17 @@
+//
+//  LavenderApp.swift
+//  Lavender
+//
+//  Created by Nicholas Guerrero on 5/23/23.
+//
+
+import SwiftUI
+
+@main
+struct LavenderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
