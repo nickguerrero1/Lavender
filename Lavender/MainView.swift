e8358fe3887df802d322f051f37308c3e1bf6e8e
Nicholas Guerrero <nicholasguerrero@Nicholass-MacBook-Air.local>
2023-05-25 00:16:49 -0400

    scrollable

--- Lavender/MainView.swift
+++ Lavender/MainView.swift
@@ -1,31 +1,36 @@
-//
-//  MainView.swift
-//  Lavender
-//
-//  Created by Nicholas Guerrero on 5/23/23.
-//
-
 import SwiftUI
 
 struct MainView: View {
     
     var body: some View {
-        VStack{
-            Text("You are logged in")
-                .bold()
-                .frame(width: 300, height: 50)
-                .background(Color.purple.opacity(0.12))
-                .cornerRadius(50)
-            Spacer()
-            Image("Lavender")
-                .resizable()
-                .frame(width: 250, height: 250)
-                .cornerRadius(50)
-                .padding(.bottom, 60)
-            Text("Lavender")
-                .font(.title)
-            Spacer()
-        }
+        ScrollView(.vertical, showsIndicators: false, content:{
+            VStack{
+                Image("Lavender")
+                    .resizable()
+                    .frame(width: 250, height: 250)
+                    .cornerRadius(50)
+                    .padding(.bottom, 60)
+                Text("Lavender")
+                    .font(.title)
+                Spacer()
+                Image("Lavender")
+                    .resizable()
+                    .frame(width: 250, height: 250)
+                    .cornerRadius(50)
+                    .padding(.bottom, 60)
+                Text("Lavender")
+                    .font(.title)
+                Spacer()
+                Image("Lavender")
+                    .resizable()
+                    .frame(width: 250, height: 250)
+                    .cornerRadius(50)
+                    .padding(.bottom, 60)
+                Text("Lavender")
+                    .font(.title)
+                Spacer()
+            }
+        })
     }
 }
 
