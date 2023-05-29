bb2e97c06481405b5ed03aa4e567d2e1287a6c08
Nicholas Guerrero <nicholasguerrero@Nicholass-MacBook-Air.local>
2023-05-28 21:17:32 -0400

    scrollview on flowerview

--- Lavender/FlowerView.swift
+++ Lavender/FlowerView.swift
@@ -6,30 +6,45 @@ struct FlowerView: View {
     let images = [Image("Leaf1"), Image("Leaf2"), Image("Leaf3"), Image("Leaf4"), Image("Leaf5"), Image("Leaf6"), Image("Leaf7"), Image("Leaf8")]
     
     var body: some View {
-        VStack{
-            HStack{
-                VStack{
-                    ForEach(0..<8) { index in
-                        ZStack {
-                            Rectangle()
-                                .frame(width: 70, height: 25)
-                                .foregroundColor(.green.opacity(0.20 + Double(index) * 0.10))
-                                .cornerRadius(30)
-                            HStack{
-                                images[index]
-                                    .resizable()
-                                    .frame(width: 20, height: 20)
-                                Text("\(rarity[index])")
-                                    .bold()
-                                    .font(.system(size: 15))
+        ZStack{
+            VStack{
+                HStack{
+                    VStack{
+                        ForEach(0..<8) { index in
+                            ZStack {
+                                Rectangle()
+                                    .frame(width: 70, height: 25)
+                                    .foregroundColor(.green.opacity(0.20 + Double(index) * 0.10))
+                                    .cornerRadius(30)
+                                HStack{
+                                    images[index]
+                                        .resizable()
+                                        .frame(width: 20, height: 20)
+                                    Text("\(rarity[index])")
+                                        .bold()
+                                        .font(.system(size: 15))
+                                }
                             }
                         }
                     }
+                    .padding(.leading)
+                    Spacer()
                 }
-                .padding(.leading)
                 Spacer()
             }
-            Spacer()
+            ScrollView {
+                HStack{
+                    VStack{
+                        Rectangle()
+                            .frame(width: 250, height: 60)
+                        Rectangle()
+                            .frame(width: 250, height: 60)
+                        Rectangle()
+                            .frame(width: 250, height: 60)
+                    }
+                    .padding(.leading, 80)
+                }
+            }
         }
         .onAppear {
             DataFetcher.loadPetalCount { fetchedRarity in
